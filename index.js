ab44dc37a23b89e82aa4b2fbb744c21c8b1b3795
CraigAdumuah <Craigadumuah123@gmail.com>
2025-03-12 22:14:32 +0000

    mc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
+import App from "./Frontend/src/components/App.jsx";
 import "./styles/App.css";
 
 ReactDOM.render(
